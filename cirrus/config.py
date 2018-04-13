50434367bb6929c852f611bde13871d5bce6945e
philloooo <phillis.tt@gmail.com>
2018-04-13 10:22:35 -0500

    feat(config): switch to object

--- cirrus/config.py
+++ cirrus/config.py
@@ -3,29 +3,77 @@ cirrus Configuration
 
 - Contains the necessary information to manage a google project, including
   some info about it and credentials. Usually the credentials supplied
-  would be some sort of "admin" service account on the project not used
+  would be some sort of 'admin' service account on the project not used
   by anyone else.
 """
 
 import os
 
-# The unique ID for the Google Cloud Project to manage by default
-GOOGLE_PROJECT_ID = os.environ.get("GOOGLE_PROJECT_ID", "").strip("\"")
 
-# Path to credentialso for accessing the Google Cloud Project
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", "").strip("\"")
+class Config(object):
+    """
+    Configuration singleton that's instantiated on module load
+    Allows dynamically update configurations from .update() or
+    setting environment variables
+    """
+    def __init__(self, **kwargs):
+        self.configs = kwargs
 
-# Admin email for Google Cloud Project
-GOOGLE_ADMIN_EMAIL = os.environ.get("GOOGLE_ADMIN_EMAIL", "").strip("\"")
+    def update(self, **kwargs):
+        """
+        update configuration properties
+        """
+        self.configs.update(kwargs)
 
-# Domain for group management
-GOOGLE_IDENTITY_DOMAIN = os.environ.get("GOOGLE_IDENTITY_DOMAIN", "").strip("\"")
+    @property
+    def SERVICE_KEY_EXPIRATION_IN_DAYS(self):
+        return self.configs.get(
+            'SERVICE_KEY_EXPIRATION_IN_DAYS', 10)
 
-# Admin email for admin domain-wide service account to act for
-GOOGLE_CLOUD_IDENTITY_ADMIN_EMAIL = os.environ.get("GOOGLE_CLOUD_IDENTITY_ADMIN_EMAIL", "").strip("\"")
+    @property
+    def GOOGLE_PROJECT_ID(self):
+        # The unique ID for the Google Cloud Project to manage by default
+        return os.environ.get(
+            'GOOGLE_PROJECT_ID',
+            self.configs.get('GOOGLE_PROJECT_ID', '')).strip('\'')
 
-# API key to use during API calls
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY", "").strip("\"")
+    @property
+    def GOOGLE_APPLICATION_CREDENTIALS(self):
+        # Path to credentialso for accessing the Google Cloud Project
+        return os.environ.get(
+            'GOOGLE_APPLICATION_CREDENTIALS',
+            self.configs.get('GOOGLE_APPLICATION_CREDENTIALS', '')
+                ).strip('\'')
 
-# Maximum life for a service key
-SERVICE_KEY_EXPIRATION_IN_DAYS = 10
+    @property
+    def GOOGLE_ADMIN_EMAIL(self):
+        # Admin email for Google Cloud Project
+        return os.environ.get(
+            'GOOGLE_ADMIN_EMAIL',
+            self.configs.get('GOOGLE_ADMIN_EMAIL', '')).strip('\'')
+
+    @property
+    def GOOGLE_IDENTITY_DOMAIN(self):
+        # Domain for group management
+        return os.environ.get(
+            'GOOGLE_IDENTITY_DOMAIN',
+            self.configs.get('GOOGLE_IDENTITY_DOMAIN', '')).strip('\'')
+
+    @property
+    def GOOGLE_CLOUD_IDENTITY_ADMIN_EMAIL(self):
+        # Admin email for admin domain-wide service account to act for
+        return os.environ.get(
+            'GOOGLE_CLOUD_IDENTITY_ADMIN_EMAIL',
+            self.configs.get('GOOGLE_CLOUD_IDENTITY_ADMIN_EMAIL', '')
+            ).strip('\'')
+
+    @property
+    def GOOGLE_API_KEY(self):
+        # API key to use during API calls
+        return os.environ.get(
+            'GOOGLE_API_KEY',
+            self.configs.get('GOOGLE_API_KEY', '')).strip('\'')
+
+
+
+config = Config()
