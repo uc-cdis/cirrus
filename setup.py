c95b82e1deb9811fb102bf639a8a43d91c904b34
avantol <avantol@uchicago.edu>
2018-05-03 14:26:39 -0500

    fix(deps): upgrade deps for google

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.5',
     install_requires=[
         "google-cloud>=0.30.0",
-        "google-api-python-client>=1.6.4",
+        "google-api-python-client>=1.6.7",
         "google-auth-httplib2>=0.0.3",
         "oauth2client<4.0dev,>=2.0.0"
     ],
