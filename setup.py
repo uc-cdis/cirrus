11036afcd1fa4605644c8c5eba0d79070c866088
avantol <avantol@uchicago.edu>
2018-05-03 14:01:30 -0500

    fix(update-setup): update setup.py with new deps

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='cirrus',
-    version='0.0.1',
+    version='0.0.5',
     install_requires=[
-        "google-cloud==0.30.0",
-        "google-api-python-client==1.6.4",
+        "google-cloud>=0.30.0",
+        "google-api-python-client>=1.6.4",
+        "google-auth-httplib2>=0.0.3",
         "oauth2client<4.0dev,>=2.0.0"
     ],
     include_package_data=True,
