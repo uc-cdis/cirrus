248d68657e6a9677d11a724ee0935c40c13cd314
avantol <avantol@uchicago.edu>
2018-05-10 12:17:18 -0500

    fix(deps): fix dependancies so it installs

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name='cirrus',
     version='0.0.5',
     install_requires=[
+        "oauth2client<4.0dev,>=2.0.0",
         "google-cloud>=0.30.0",
         "google-api-python-client>=1.6.7",
-        "google-auth-httplib2>=0.0.3",
-        "oauth2client<4.0dev,>=2.0.0"
+        "google-auth>=1.4.1",
+        "google-auth-httplib2>=0.0.3"
     ],
     include_package_data=True,
     packages=["cirrus", "cirrus.google_cloud"],
