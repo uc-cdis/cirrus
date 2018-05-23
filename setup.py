b03c5fb898f1700c4283380547d0e5706320efbf
avantol <avantol@uchicago.edu>
2018-05-23 09:34:58 -0500

    fix(deps): use new google recommended lib (deprecating old one)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.5',
     install_requires=[
         "oauth2client<4.0dev,>=2.0.0",
-        "google-cloud>=0.30.0,<0.33.0",
+        "google-cloud-storage>=1.10.0",
         "google-api-python-client>=1.6.7",
         "google-auth>=1.4.1",
         "google-auth-httplib2>=0.0.3"
