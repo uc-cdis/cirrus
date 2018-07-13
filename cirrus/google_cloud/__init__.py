7f7192c9861d6c727f5313de87543c615dc3fd18
Benjamin Rohrer <benJrohrer@gmail.com>
2018-07-13 13:14:01 -0500

    expose service account type variables

--- cirrus/google_cloud/__init__.py
+++ cirrus/google_cloud/__init__.py
@@ -1,2 +1,8 @@
-from .manager import GoogleCloudManager
+from .manager import (
+    GoogleCloudManager,
+    COMPUTE_ENGINE_DEFAULT,
+    GOOGLE_API,
+    COMPUTE_ENGINE_API,
+    USER_MANAGED,
+)
 from .utils import get_valid_service_account_id_for_client
