9cd6b70be3a50b94d0c1b907528386c72a77d31f
Michael Martinson <mjmartinson@uchicago.edu>
2020-05-27 11:56:07 -0700

    dev tests

--- cirrus/google_cloud/utils.py
+++ cirrus/google_cloud/utils.py
@@ -342,7 +342,7 @@ def get_signed_url(
     )
 
     # signature = binascii.hexlify(creds.signer.sign(string_to_sign)).decode()
-    signature = binascii.hexlify(creds.sign_blob(string_to_sign)).decode()
+    signature = binascii.hexlify(creds.sign_blob(string_to_sign)[1]).decode()
 
     scheme_and_host = "{}://{}".format("https", host)
     signed_url = "{}{}?{}&x-goog-signature={}".format(
