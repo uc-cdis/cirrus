198fdb214a5d8acc4bd0954e3f38e0f18c3d301c
mattreex <mattreex.9@gail.com>
2021-06-09 11:34:20 -0500

    cdislogging

--- cirrus/google_cloud/utils.py
+++ cirrus/google_cloud/utils.py
@@ -6,17 +6,16 @@ import json
 import hashlib
 import re
 from urllib.parse import quote, urlencode
-import logging
 
 from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
-
+from cdislogging import get_logger
 
 from cirrus.config import config
 from cirrus.google_cloud.errors import GoogleNamingError
 
 GOOGLE_SERVICE_ACCOUNT_REGEX = "[a-z][a-z\d\-]*[a-z\d]"
-logging.basicConfig(level=logging.INFO)
+logger = get_logger(__name__, log_level="info")
 
 
 def get_valid_service_account_id_for_user(user_id, username, prefix=""):
@@ -275,7 +274,7 @@ def get_signed_url(
     creds = service_account.Credentials.from_service_account_info(service_account_creds)
 
     bucket_name = path_to_resource.split("/")[0]
-    logging.info("Generating URL for bucket name: {}".format(bucket_name))
+    logger.info("Generating URL for bucket name: {}".format(bucket_name))
     object_name = "/".join(path_to_resource.split("/")[1:])
     escaped_object_name = quote(object_name.encode(), safe=b"/~")
     canonical_uri = "/{}".format(escaped_object_name)
